8e23d664e553085cb443889f889dcf8d6b6876cd
okharyton <olegkhacv@gmail.com>
2024-02-28 23:02:26 +0200

    feat: add first part of useReducer logic (dataRecieved, dataFailed, default cases)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
 import "./App.css";
 import Header from "./assets/components/Header.jsx";
 import Main from "./assets/components/Main.jsx";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action is unknown");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
