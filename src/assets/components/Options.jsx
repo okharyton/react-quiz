61c5f1cab8cbec7dc5cb307293735269e152d869
okharyton <olegkhacv@gmail.com>
2024-02-29 22:42:52 +0200

    feat: add part of correct/wrong answer logic + points calc state

--- src/assets/components/Options.jsx
+++ src/assets/components/Options.jsx
@@ -1,9 +1,15 @@
-export default function Options({ question }) {
+export default function Options({ question, dispatch, answer }) {
   console.log(question);
+  const hasAnswered = answer !== null;
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button key={option} className="btn btn-option">
+      {question.options.map((option, index) => (
+        <button
+          key={option}
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${hasAnswered ? (index === question.correctOption ? "correct" : "wrong") : ""}`}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+          disabled={hasAnswered}
+        >
           {option}
         </button>
       ))}
